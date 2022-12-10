543e5f3511c68e3d9c8ccd51702563ff522bb574
Sakari Klemola <sakari.klemola@tuni.fi>
2022-12-10 18:31:13 +0200

    update filterTest

--- test/filterTest.js
+++ test/filterTest.js
@@ -1,22 +1,26 @@
 import { expect } from "chai";
 import filter from "../src/filter.js";
 
-const testArray = [
-    {'name': 'potato', 'price': 1.00, 'year': 2022},
-    {'name': 'fridge', 'price': 200.00, 'year': 2000},
-    {'name': 'potato', 'price': 1.00, 'year': 2022},
-    {'name': 'potato', 'price': 1.00, 'year': 2022},
-    {'name': 'potato', 'price': 1.00, 'year': 2022},
-    {'name': 'potato', 'price': 1.00, 'year': 2022},
-    {'name': 'potato', 'price': 1.00, 'year': 2022},
-    {'name': 'potato', 'price': 1.00, 'year': 2022},
-    {'name': 'potato', 'price': 1.00, 'year': 2022},
-    {'name': 'potato', 'price': 1.00, 'year': 2022},
+const users = [
+   { 'user': 'barney', 'active': true },
+   { 'user': 'fred',   'active': false } 
 ];
 
 
-    describe("Filter", () => {
-        it("shoud return an empty array for an empty array", function () {
-            expect(filter([], ({ price }) => price > 0.00)).to.equal([[]]);
-        });
+describe("Filter", () => {
+    it("shoud return an empty array for an empty array", function () {
+        expect(filter([[]], ({ active }) => active)).to.eql([[]]);
+    });
+    it("should return input array if predicate is not given", function () {
+        expect(filter(users).to.eql(users));
+    });
+    it("shoud return an empty array for a non-array", function () {
+        expect(filter("foo", ({ passive }) => passive)).to.eql([[]]);
+    });
+    it("return all active users", function () {
+        expect(filter(users, ({ active }) => active)).to.eql([{ 'user': 'barney', 'active': true }]);
+    });
+    it("should return empty array if predicate doesn't match", function () {
+        expect(filter(users, ({passive}) => passive)).to.eql([[]]);
+    });
 });
